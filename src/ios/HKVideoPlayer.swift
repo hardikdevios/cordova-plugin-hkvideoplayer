0bf29ab6fde76966c6d8cbc85e7bf21f122112ac
Hardik Shah <hardikrocks111@gmail.com Primary  Private hardikrocks111@gmail.com>
2017-06-04 13:37:05 +0530

    First Commit

---
+++ src/ios/HKVideoPlayer.swift
@@ -0,0 +1,96 @@
+//
+//  VideoViewController.swift
+//  Doctodoor
+//
+//  Created by Hardik on 22/08/15.
+//  Copyright (c) 2015 PRAXINFO. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+import AVKit
+import AVFoundation
+//import Cordova
+
+@objc(HKVideoPlayer) class HKVideoPlayer: CDVPlugin {
+    var playerViewController:AVPlayerViewController!
+    override func pluginInitialize() {
+        playerViewController = AVPlayerViewController()
+        
+    }
+    
+    @objc(play:)
+    func play(command:CDVInvokedUrlCommand){
+        guard let path = command.argument(at: 0) as? String else {
+            return
+        }
+        
+        let videoURL = URL(fileURLWithPath: path)
+        let player = AVPlayer(url: videoURL)
+        self.present(player: player)
+        
+    }
+    @objc(playLocal:)
+    func playLocal(command:CDVInvokedUrlCommand){
+        guard let name = command.argument(at: 0) as? String else {
+            return
+        }
+        let ext = name.fileExtension()
+        let file = name.fileName()
+        let path = Bundle.main.path(forResource: file, ofType: ext, inDirectory: "www/assets/video")
+        let videoURL = URL(fileURLWithPath: path!)
+        let player = AVPlayer(url: videoURL)
+        self.present(player: player)
+        
+    }
+    @objc(stream:)
+    func stream(command:CDVInvokedUrlCommand){
+        guard let url = command.argument(at: 0) as? String else {
+            return
+        }
+        
+        let videoURL = URL(string: url)
+        let player = AVPlayer(url: videoURL!)
+        self.present(player: player)
+        
+    }
+    private func present(player:AVPlayer){
+        
+        playerViewController.player = player
+        
+        DispatchQueue.main.async {
+            self.viewController.present(self.playerViewController, animated: true) {
+                self.playerViewController.player!.play()
+            }
+            
+        }
+        
+    }
+    
+    deinit {
+        self.playerViewController = nil
+    }
+    
+    
+    
+}
+extension String {
+    
+    func fileName() -> String {
+        
+        if let fileNameWithoutExtension = NSURL(fileURLWithPath: self).deletingPathExtension?.lastPathComponent {
+            return fileNameWithoutExtension
+        } else {
+            return ""
+        }
+    }
+    
+    func fileExtension() -> String {
+        
+        if let fileExtension = NSURL(fileURLWithPath: self).pathExtension {
+            return fileExtension
+        } else {
+            return ""
+        }
+    }
+}
---
+++ www/HKVideoPlayer.js
@@ -0,0 +1,18 @@
+var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+
+var HKVideoPlayer = function() {
+};
+
+HKVideoPlayer.play = function(url) {
+  exec(null, null, 'HKVideoPlayer', 'play', [url]);
+};
+HKVideoPlayer.stream = function(url) {
+    exec(null, null, 'HKVideoPlayer', 'stream', [url]);
+};
+HKVideoPlayer.playLocal = function(url) {
+    exec(null, null, 'HKVideoPlayer', 'playLocal', [url]);
+};
+module.exports = HKVideoPlayer;
